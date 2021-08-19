4c3c5128d4ea2f2a2d087e1e3b6ff76032b0c9d6
nlfmt <nlfmt@gmx.de>
2021-08-19 13:57:07 +0200

    add procfile

--- scripts/sender.py
+++ scripts/sender.py
@@ -1,7 +1,7 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("94.134.180.1", 1337))
+s.connect(("34.247.182.205", 52128))
 
 msg = s.recv(1024)
 print(msg)
--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.bind(("0.0.0.0", 1234))
 s.listen(5)
+print("Listening!")
+
+
 
 while True:
     client_socket, addr = s.accept()
+    print(f"Client with address {addr} connected")
     client_socket.send(bytes(f"You connected from {addr}", "utf-8"))
     exit()
