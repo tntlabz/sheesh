bebdd8d66d33ff89f170388c32a2b877b40d5a14
nlfmt <nlfmt@gmx.de>
2021-08-20 14:53:36 +0200

    bissl infos reingeschrieben

--- server/scripts/sender.py
+++ server/scripts/sender.py
@@ -3,9 +3,14 @@ import websockets
 
 
 async def hello():
+
+    # das "wss://" am anfang sorgt dafür, dass der server weiss, dass man ne verbindung aufbauen will
+    # es gibt auch "ws://", das ist quasi wie http und https, wss ist irgendwie sicherer.
     uri = "wss://sheesh-server.herokuapp.com/0.0.0.0"
+
     async with websockets.connect(uri) as websocket:
         await websocket.send("Hello world!")
+
         msg = await websocket.recv()
         print(f"Got response: {msg}")
 
