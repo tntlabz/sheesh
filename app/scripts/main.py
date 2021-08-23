36252708b611f2d6c9389c55ce9749231eb62be6
TimV11 <t@4vielhauer.de>
2021-08-23 23:48:55 +0200

    mongodb

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -1,8 +1,24 @@
-import asyncio, time
+import asyncio, time, sys, sysconfig, pymongo
 
 from ipc import recv_msg, send_msg
 
-   
+
+# gets username & password from console
+if sys.argv[0] == __file__:
+    username, password = sys.argv[1:]
+else:
+    username, password = sys.argv[0:]
+
+
+# create DB connection
+CONNECTION_STRING = sysconfig.get_config_var("MONGODB_URI")
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client["users"]
+collection = db["user_collection"]
+user = {"username" : username, "password": password}
+collection.insert_one(user)
+
+
 async def listen_for_input():
     while True:
         send_msg("MSG: ", await recv_msg())
